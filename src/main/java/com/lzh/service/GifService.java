96bbe3ba15da5161ba0aa764f604e88ddeb79033
zhangpeng <peng.zhang33@ele.me>
2019-09-05 22:58:00 +0800

    test

--- src/main/java/com/lzh/service/GifService.java
+++ src/main/java/com/lzh/service/GifService.java
@@ -37,7 +37,7 @@ public class GifService {
         String assPath = renderAss(subtitles);
         String gifPath = Paths.get(tempPath).resolve(UUID.randomUUID() + ".gif").toString();
         String videoPath = Paths.get(tempPath).resolve(subtitles.getTemplateName()+"/template.mp4").toString();
-        String cmd = String.format("ffmpeg -i %s -r 6 -vf ass=%s,scale=300:-1 -y %s", videoPath, assPath, gifPath);
+        String cmd = String.format("ffmpeg -i %s -r 6 -vf ass=%s,scale=300:-1 -y %s > Log.log 2>&1 &", videoPath, assPath, gifPath);
         if ("simple".equals(subtitles.getMode())) {
 //            cmd = String.format("ffmpeg -i %s -r 2 -vf ass=%s,scale=250:-1 -f gif - |gifsicle --optimize=3 --delay=20 > %s ", videoPath, assPath, gifPath);
             cmd = String.format("ffmpeg -i %s -r 5 -vf ass=%s,scale=180:-1 -y %s ", videoPath, assPath, gifPath);
@@ -46,7 +46,7 @@ public class GifService {
         try {
             Process exec = Runtime.getRuntime().exec(cmd);
             exec.waitFor();
-//            logger.info("输出:{}",IOUtils.toString(exec.getErrorStream()));
+            logger.info("执行完毕");
         } catch (Exception e) {
             logger.error("生成gif报错：{}", e);
         }
